116204d2fabbf176a6705282479412ee869009e2
Allit <allitwin25@gmail.com>
2020-07-30 15:32:54 +0300

    add functions chart2

--- mainAppDiplom/mainAppDiplom/Form1.cs
+++ mainAppDiplom/mainAppDiplom/Form1.cs
@@ -23,17 +23,24 @@ namespace mainAppDiplom
             print_data_others();
             print_data_all();
             print_data_vvp();
+            create_Series_Chart2();
 
-            chart1.Titles.Add("Дані відображені у відсотках");
+
+
         }
         #region functions data
+
+        int[] data_year = new int[11];
         public void print_year()
         {
             label10.Text = "";
-            for (int i = 2009; i <= 2019; i++)
+
+            for (int i = 0; i < data_year.Length; i++)
             {
-                label10.Text += i + Environment.NewLine + Environment.NewLine;
-                comboBox1.Items.Add(i);
+                int index = 2009;
+                data_year[i] = index+i;
+                label10.Text += data_year[i] + Environment.NewLine + Environment.NewLine;
+                comboBox1.Items.Add(data_year[i]);
             }
         }
         double[] data_own = new double[11];
@@ -96,13 +103,15 @@ namespace mainAppDiplom
                 label15.Text += Environment.NewLine + Environment.NewLine;
             }
         }
+        double[] data_vvp = new double[11];
         public void print_data_vvp()
         {
             label16.Text = "";
             Random rand = new Random();
             for (int i = 0; i <= 10; i++)
             {
-                label16.Text += Math.Round(rand.Next(92500, 4800000) + rand.NextDouble(), 1);
+                data_vvp[i] = Math.Round(rand.Next(92500, 4800000) + rand.NextDouble(), 1);
+                label16.Text += data_vvp[i];
                 label16.Text += Environment.NewLine + Environment.NewLine;
             }
         }
@@ -111,6 +120,7 @@ namespace mainAppDiplom
         #region chart
         private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
         {
+            chart1.Titles.Add("Дані відображені у відсотках");
             chart1.Series["S_Year"].Points.Clear();
             int index;
             index = Convert.ToInt32(comboBox1.SelectedIndex.ToString());
@@ -128,6 +138,112 @@ namespace mainAppDiplom
             chart1.Series["S_Year"].Points.AddXY("Інщі джерела", otherspercent);
 
         }
+
+        public void create_Series_Chart2()
+        {
+            chart2.Series.Add(new Series("Власні")
+            {
+                ChartType = SeriesChartType.Spline,
+                Color = Color.Red
+            });
+
+            chart2.Series.Add(new Series("Державний бюджет")
+            {
+                ChartType = SeriesChartType.Spline,
+                Color = Color.Green
+            });
+
+            chart2.Series.Add(new Series("Іноземних інвесторів")
+            {
+                ChartType = SeriesChartType.Spline,
+                Color = Color.Blue
+            });
+
+            chart2.Series.Add(new Series("Інщі джерела")
+            {
+                ChartType = SeriesChartType.Spline,
+                Color = Color.Orange
+            });
+
+            chart2.Series.Add(new Series("Залежність ВВП/Інвестиції")
+            {
+                ChartType = SeriesChartType.Point,
+                Color = Color.BlueViolet
+            });
+            chart2.Series.Add(new Series("Лінійна регресія")
+            {
+                ChartType = SeriesChartType.Point,
+                Color = Color.BlueViolet
+            });
+        }
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
+            chart2.Series["Власні"].Points.Clear();
+            chart2.Series["Державний бюджет"].Points.Clear();
+            chart2.Series["Іноземних інвесторів"].Points.Clear();
+            chart2.Series["Інщі джерела"].Points.Clear();
+            chart2.Series["Залежність ВВП/Інвестиції"].Points.Clear();
+
+            string chart_name = comboBox2.SelectedItem.ToString();
+
+            label18.Text = "Вісь X - РІК";
+            label19.Text = "Вісь Y - млн грн";
+            if(chart_name == "Загальний")
+            {
+                for (int i = 0; i < data_year.Length; i++)
+                {
+                    chart2.Series["Власні"].Points.AddXY(data_year[i], data_own[i]);
+                    chart2.Series["Державний бюджет"].Points.AddXY(data_year[i], data_state[i]);
+                    chart2.Series["Іноземних інвесторів"].Points.AddXY(data_year[i], data_foregn[i]);
+                    chart2.Series["Інщі джерела"].Points.AddXY(data_year[i], data_others[i]);
+                }
+            }
+
+            if(chart_name == "Власні")
+            {
+                for (int i = 0; i < data_year.Length; i++)
+                {
+                    chart2.Series["Власні"].Points.AddXY(data_year[i], data_own[i]);
+                }
+            }
+            if(chart_name == "Державний бюджет")
+            {
+                for (int i = 0; i < data_year.Length; i++)
+                {
+                    chart2.Series["Державний бюджет"].Points.AddXY(data_year[i], data_state[i]);
+                }
+            }
+
+            if (chart_name == "Іноземних інвесторів")
+            {
+                for (int i = 0; i < data_year.Length; i++)
+                {
+                    chart2.Series["Іноземних інвесторів"].Points.AddXY(data_year[i], data_foregn[i]);
+                }
+            }
+            if (chart_name == "Інщі джерела")
+            {
+                for (int i = 0; i < data_year.Length; i++)
+                {
+                    chart2.Series["Інщі джерела"].Points.AddXY(data_year[i], data_others[i]);
+                }
+            }
+            //Залежність ВВП від інвестицій
+            if (chart_name == "Залежність ВВП/Інвестиції")
+            {
+                for (int i = 0; i < data_all.Length; i++)
+                {
+                    chart2.Series["Залежність ВВП/Інвестиції"].Points.AddXY(data_all[i], data_vvp[i]);
+                }
+            }
+        }
         #endregion
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
     }
 }
